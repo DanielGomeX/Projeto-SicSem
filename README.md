f9a3e7a4a909f830d96585a7b490ae3d775aa9b8
Odenilson Marques <odmarquesgm@gmail.com>
2019-09-15 22:14:08 -0300

    Revert "foram feitas alterações nos arquivos de conexao e README.md"
    This reverts commit 87ef8c6d04e80158c9f5b21142be51e26f15c79f.

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Sistema de controle de processos, licenças, notificações,  auto de infraçõe e autorizações
 
 as tecnologias que estão sendo utilizadas para o desenvilmento desse projeto são: php, sql, html5, css, bootstrap, jquery e ajax
-
-teste reverte
--- config/conexao.php
+++ config/conexao.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "bd_sema";
+$database = "bd_semam";
 /*or die, serve para nos avisar qual o problema*/
 $con = mysqli_connect($server,$user,$password,$database) or die("Erro Ao Conectar");//Serve para abrir um conexão com o banco de dados MySQL.
 mysqli_select_db($con,$database) or die(mysqli_error($con));//Serve para selecionar o banco de dados.
