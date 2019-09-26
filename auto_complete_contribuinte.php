07fe012da0989688c45f59719af52b31e46c2c57
Odenilson Marques <odmarquesgm@gmail.com>
2019-09-26 09:25:12 -0300

    foi adicionado o arquivo auto_complete_contribuinte para lista os contribuintes ao cadasdastrar uma empresa

--- auto_complete_contribuinte.php
+++ auto_complete_contribuinte.php
@@ -1,16 +1,21 @@
 <?php
 
-
 require './config/conexao.php';
 
-if(isset($_POST['search'])){
+if (isset($_POST['search'])) {
     $search = $_POST['search'];
 
     $query = "SELECT * FROM tb_empresa WHERE razaosocial_pessoafisica like '$search%' limit 5";
-    $result = mysqli_query($con,$query);
-    while($row = mysqli_fetch_array($result) ){
-        $response[] = array("value"=>$row['razaosocial_pessoafisica'],"label"=>$row['razaosocial_pessoafisica']);
+    $result = mysqli_query($con, $query);
+
+    while ($row = mysqli_fetch_array($result)) {
+
+        $empresa = $row['codigo_empresa'];
+
+        $response[] = array("value" => $row['razaosocial_pessoafisica'], "label" => $row['razaosocial_pessoafisica']);
     }
+
+
     echo json_encode($response);
 }
 exit;
