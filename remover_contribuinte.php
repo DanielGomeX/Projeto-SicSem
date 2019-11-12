c9ecaf3fed9cd1611e33f3f03b4e9b254368c2a2
odenilson marques <odmarquesgm@gmil.com>
2019-11-12 17:30:22 -0300

    foi incluido a ação de gravar quem excluiu um determinado registro

--- remover_contribuinte.php
+++ remover_contribuinte.php
@@ -14,10 +14,25 @@ if (isset($_SESSION['email']) && empty($_SESSION['email']) == FALSE) {
 }
 
 if (isset($_GET['codigo_empresa']) && empty($_GET['codigo_empresa']) == FALSE) {
-    $codigo = $_GET['codigo_empresa']; /* link dinamico utilizando o get */
+    $codigo = $_GET['codigo_empresa'];/*link dinamico utilizando o get*/
 
+    $sqlContribuinte = "SELECT *FROM tb_empresa WHERE codigo_empresa = $codigo";
+    $sqlexe = mysqli_query($con, $sqlContribuinte);
+    $info = mysqli_fetch_array($sqlexe);
+    $nomeContribuinte = $info['razaosocial_pessoafisica'];
+    
     $sql = "DELETE FROM tb_empresa WHERE codigo_empresa = $codigo";
     $exe_sql = mysqli_query($con, $sql);
+
+    //O CÓDIGO ABAIXO REGISTRA O USUARIO QUE REALIZOU O CADASTRO DE CERTO EMPRESA / PESSOA FISICA
+    $emailUser = $_SESSION['email'];
+    $user = $_SESSION['nome'];
+    $ip_rem = getenv('REMOTE_ADDR'); //pega o ip da maquina ususario
+    $ip_maq = $_SERVER["REMOTE_ADDR"]; //Pego o IP
+    $data = Date("Y-m-d H:i:s");
+    $acaoUsuario = "Excluiu o contribuinte->$nomeContribuinte";
+    $sqlLog = "INSERT INTO tb_controle_usuario(acao,data_acesso,ip_maquina,ip_remoto,email,nome)VALUES(UPPER('$acaoUsuario'),'$data','$ip_maq','$ip_rem','$emailUser','$user')";
+    mysqli_query($con, $sqlLog);
 
     if (mysqli_affected_rows($con) > 0) {
         $_SESSION['msg'] = "<div class='alert alert-success text-center' role='alert'><strong>CONTRIBUINTE EXCLUÍDO COM SUCESSO!</strong></div>";
