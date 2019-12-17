324691ad7ba5bcd71295399bc45da1a46fad80a3
odenilson marques <odmarquesgm@gmail.com>
2019-12-17 11:37:53 -0300

    novas alterações feitas na tela de detalhes_empresa;

--- exibe_empr_den.php
+++ exibe_empr_den.php
@@ -18,21 +18,14 @@ $sql = ("SELECT tb_empreendimento.codigo_empreendimento,tb_empreendimento.nome_e
 //$sql = ("SELECT codigo_empresa,nome_fantasia FROM tb_empresa WHERE codigo_empresa = ".$codigo_empresas." UNION SELECT codigo_empreendimento,nome_empreendimento FROM tb_empreendimento WHERE codigo_empreendimento = fk1_codigo_empresa");
 //$sql = ("SELECT  *FROM tb_empreendimento WHERE fk1_codigo_empresa=" . $codigo_empresas . " ORDER BY nome_empreendimento DESC limit 1");
 
-$sql = "SELECT tb_empresa.codigo_empresa,tb_empresa.nome_fantasia,tb_empreendimento.codigo_empreendimento,tb_empreendimento.nome_empreendimento FROM tb_empreendimento,tb_empresa
-        WHERE 
-tb_empreendimento.fk1_codigo_empresa  = tb_empresa.codigo_empresa and codigo_empresa = $codigo_empresas";   
+//$sql = "SELECT tb_empresa.codigo_empresa,tb_empresa.nome_fantasia,tb_empreendimento.codigo_empreendimento,tb_empreendimento.nome_empreendimento FROM tb_empreendimento,tb_empresa
+//        WHERE 
+//tb_empreendimento.fk1_codigo_empresa  = tb_empresa.codigo_empresa and codigo_empresa = $codigo_empresas";
 
 $recebesql = (mysqli_query($con, $sql));
 
 while ($linha = mysqli_fetch_array($recebesql)) {
-    
-        
-        echo"<option value='" . $linha['codigo_empreendimento'] . "'>" . $linha['nome_empreendimento'] . "</option>";
-//        echo"<option value='" . $linha['codigo_empresa'] . "'>" . $linha['nome_fantasia'] . "</option>";
-    
-//        echo"<option value='" . $linha[''] . "'>" . $linha[''] . "</option>";
-//        echo"<option value='" . $linha['codigo_empresa'] . "'>" . $linha['nome_fantasia'] . "</option>";
-    
-    
+
+    echo"<option value='" . $linha['codigo_empreendimento'] . "'>" . $linha['nome_empreendimento'] . "</option>";
 }
     
